950fbf1ae5a3494a65a5c6b87b367e9d5d2ba781
gpj <1549361150@qq.com>
2019-05-06 05:46:41 +0800

    添加搜索和筛选组件样式

--- src/components/filtrate.vue
+++ src/components/filtrate.vue
@@ -8,51 +8,33 @@
                 </router-link>
             </li>
             <li>
-                <div style="text-align: center">
-                    <button @click="hideShowFilter" class="button  button-light icon-right ion-android-arrow-dropdown">
-                        筛选
-                    </button>
-                </div>
+                <button @click="hideShowFilter" class="button  button-light icon-right ion-android-arrow-dropdown">
+                    筛选
+                </button>
             </li>
-            <li>
-                <div title="搜索" v-show="isShow">
-                    <ul>
-                        <li class="cube-index-list-item">
-                            <div class="">
-                                <div>
-                                    <span>价格:</span>
-                                    <span>
-                                        <span>￥{{}}</span>&nbsp;&nbsp;至&nbsp;&nbsp;
-                                        <span>￥{{}}</span>
-                                    </span>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="cube-index-list-item">
-                            <div>
-                                <div>
-                                    <span>图书类型:</span>
-                                    <span class="book-name green"></span>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="cube-index-list-item">
-                            <div>
-                                <div>
-                                    <span>日期:</span>
-                                </div>
-                                <input type="text" readonly="readonly" placeholder="请选择开始日期">
-                                <span class="line-span">——</span>
-                                <input type="text" readonly="readonly" placeholder="请选择结束日期">
-                            </div>
-                        </li>
-
-                        <li class="cube-index-list-item">
-                            <cube-button :light="true">重置</cube-button>
-                            <cube-button :light="true">搜索</cube-button>
-                        </li>
-                    </ul>
-                </div>
+            <li title="搜索" v-show="isShow">
+                <ul>
+                    <li class="cube-index-list-item">
+                        <label>价格:</label>
+                        <input type="text" placeholder="￥起始"/>
+                        <em>&nbsp;&nbsp;至&nbsp;&nbsp;</em>
+                        <input type="text" placeholder="￥结束"/>
+                    </li>
+                    <li class="cube-index-list-item">
+                        <label>类型:</label>
+                        <input type="text" placeholder="￥结束"/>
+                    </li>
+                    <li class="cube-index-list-item">
+                        <label>日期:</label>
+                        <input type="text" placeholder="请选择开始日期">
+                        <em class="line-span">——</em>
+                        <input type="text" placeholder="请选择结束日期">
+                    </li>
+                    <li class="cube-index-list-item">
+                        <cube-button :light="true">重置</cube-button>
+                        <cube-button :light="true">搜索</cube-button>
+                    </li>
+                </ul>
             </li>
         </ul>
 
@@ -80,40 +62,70 @@
 
 <style lang="scss" module>
     @import "../assets/scss/element";
+
     .panel {
         @include panel;
-        margin: 2px 0;
+        margin-top: 0;
         & > h4 {
             display: none;
         }
         .content {
             @include flex;
             li {
-                &:first-child {
+                border: 1px solid #f5f5f5;
+                z-index: 1200;
+                &:nth-child(1) {
                     @include flex(row);
                     input {
-                        padding-left: 15px;
                         flex-grow: 1;
+                       // padding-left: 5%;
                     }
                     button {
-                        margin-right: 2px;
-                        margin-bottom: 4px;
                         color: white;
                         background-color: #56b6e7;
-                        border: 1px solid transparent; //自定义边框
+                        border: 0 solid transparent; //自定义边框
                         height: 100%;
                         width: 60px;
                         font-size: 16px;
                     }
-                    border-bottom: 5px solid #f5f5f5;
                 }
                 &:nth-child(2) {
-                    border-bottom: 5px solid #f5f5f5;
-                    margin-top: 2px;
+                    button {
+                        width: 100%;
+                    }
                 }
-                &:nth-child(3){
-                    background-color: #f5f5f5;
-                    z-index: 1200;
+                &:nth-child(3) {
+                    background-color: white;
+                    ul {
+                        @include flex;
+                        li {
+                            @include flex(row);
+                            height: 6vh;
+                            border: 0;
+                            label, em {
+                                text-align: center;
+                                width: 15%;
+                                line-height: 6vh;
+                            }
+                            input {
+                                width: 35%;
+                                height: 100%;
+                            }
+
+                            &:nth-child(2) {
+                                input {
+                                    width: 85%
+                                }
+                            }
+
+                            &:nth-child(4) {
+                                > * {
+                                    display: inline-block;
+                                    width: 50%;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
