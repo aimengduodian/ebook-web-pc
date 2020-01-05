8a78610ae7ce3e37290d86f8de269307c3a77e2d
gpj <1549361150@qq.com>
2020-01-05 12:57:00 +0800

    modify title

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ function getRouter () {
 
   //使用钩子函数对路由进行权限跳转
   Router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title} | vue-manage-system`
+    document.title = `${to.meta.title} | ebook-pc`
     const role = sessionStorage.getItem('ms_username')
     if (!role && to.path !== '/login') {
       next('/login')
